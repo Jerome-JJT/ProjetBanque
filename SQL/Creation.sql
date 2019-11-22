e62fecab711c61d7c1d3138ff7ae6a7afaa3f0f1
jeromejj <jerome.jaquemet@gmail.com>
2019-11-22 08:06:04 +0100

    Added unique attribute on email

--- SQL/Creation.sql
+++ SQL/Creation.sql
@@ -7,7 +7,7 @@ USE `Banking`;
 CREATE TABLE IF NOT EXISTS `Users` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `type` ENUM('Public','Entreprise','Admin') NOT NULL DEFAULT 'Public',
-    `email` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL UNIQUE,
     `password` VARCHAR(50) NOT NULL,
     `money` INT NOT NULL
 );
