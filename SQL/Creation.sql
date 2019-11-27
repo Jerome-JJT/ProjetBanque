9d4a9321bc29e178f10c534ea15757bd783ede3e
jeromejj <jerome.jaquemet@gmail.com>
2019-11-27 11:42:59 +0100

    Updated sql script

--- SQL/Creation.sql
+++ SQL/Creation.sql
@@ -1,13 +1,13 @@
 DROP DATABASE IF EXISTS `Banking`;
 
 CREATE DATABASE `Banking`;
-usersusersusersusersusersusersusersusers
 USE `Banking`;
 
 CREATE TABLE IF NOT EXISTS `Users` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `active` TINYINT(1) NOT NULL DEFAULT '1',
     `type` ENUM('Public','Entreprise','Admin') NOT NULL DEFAULT 'Public',
     `email` VARCHAR(50) NOT NULL UNIQUE,
-    `password` VARCHAR(50) NOT NULL,
-    `money` INT NOT NULL
+    `password` VARCHAR(80) NOT NULL,
+    `money` INT NOT NULL DEFAULT '0'
 );
