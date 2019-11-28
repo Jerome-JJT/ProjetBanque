f581646914a9eb1a3b2836e67adf0fb15533f6d3
jeromejj <jerome.jaquemet@gmail.com>
2019-11-28 14:16:22 +0100

    Added JsonManagement

--- ProjetBanque/ProjetBanque/JsonManagement.cs
+++ ProjetBanque/ProjetBanque/JsonManagement.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjetBanque
 {
-    class JsonManagement
+    /// <summary>
+    /// Extract/insert data from json
+    /// </summary>
+    public class JsonManagement
     {
+        /// <summary>
+        /// Create json reader object
+        /// </summary>
+        public JsonManagement()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public JsonData ExtractData(string path)
+        {
+            StreamReader file = new StreamReader(path);
+            return JsonConvert.DeserializeObject<JsonData>(file.ReadToEnd());
+        }
     }
 }
