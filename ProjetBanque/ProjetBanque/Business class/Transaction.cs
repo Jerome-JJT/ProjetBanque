d44a1c03bd33eecbc492fc9614dfc5830f0dba1d
jeromejj <jerome.jaquemet@gmail.com>
2019-12-19 15:05:41 +0100

    Added user and transaction classes

---
+++ ProjetBanque/ProjetBanque/Business class/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetBanque.Business_class
+{
+    /// <summary>
+    /// Store unique transaction between two users
+    /// </summary>
+    public class Transaction
+    {
+        private string date;
+        private double amount;
+        private string reason;
+
+        private string senderDefine;
+        private string receiverDefine;
+    }
+}
---
+++ ProjetBanque/ProjetBanque/Business class/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetBanque.Business_class
+{
+    /// <summary>
+    /// Store user's informations and transactions to display them
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Possibles user's accounts types
+        /// </summary>
+        public enum Types
+        {
+            /// <summary>
+            /// Normal user account
+            /// </summary>
+            Public = 1,
+            /// <summary>
+            /// Entreprise account, can use list
+            /// </summary>
+            Entreprise = 2,
+            /// <summary>
+            /// Admin account, can see every account
+            /// </summary>
+            Admin = 3
+        }
+
+        private string email;
+        private string iban;
+        private double money;
+
+        public User(string email, string iban, double money)
+        {
+            this.email = email;
+            this.iban = iban;
+            this.money = money;
+        }
+
+
+    }
+}
