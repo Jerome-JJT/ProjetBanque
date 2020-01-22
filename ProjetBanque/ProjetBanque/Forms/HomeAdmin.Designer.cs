d2d042a8bddcde0f1756e9267acb39e3bcec2416
jeromejj <jerome.jaquemet@gmail.com>
2020-01-22 08:14:17 +0100

    Update HomeAdmin

--- ProjetBanque/ProjetBanque/Forms/HomeAdmin.Designer.cs
+++ ProjetBanque/ProjetBanque/Forms/HomeAdmin.Designer.cs
@@ -263,6 +263,7 @@
             this.cmdModifyMonney.TabIndex = 27;
             this.cmdModifyMonney.Text = "Modifier argent utilisateur";
             this.cmdModifyMonney.UseVisualStyleBackColor = true;
+            this.cmdModifyMonney.Click += new System.EventHandler(this.cmdModifyMonney_Click);
             // 
             // FrmHomeAdmin
             // 
@@ -277,6 +278,9 @@
             this.Controls.Add(this.cmdProfil);
             this.Controls.Add(this.cmdExit);
             this.Controls.Add(this.lblEmail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(827, 612);
+            this.MinimumSize = new System.Drawing.Size(827, 612);
             this.Name = "FrmHomeAdmin";
             this.Text = "Accueil Administrateur";
             ((System.ComponentModel.ISupportInitialize)(this.datHistory)).EndInit();
